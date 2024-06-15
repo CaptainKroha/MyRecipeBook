9908d703dd35345f0a2ff3393d0f2f691ae0386f
CaptainKroha <dzokvinton@gmail.com>
2024-06-15 21:30:26 +0300

    Add Product classes

--- items.h
+++ items.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+
+
+class Product {
+	std::string name;
+
+public:
+	Product(std::string _name):name(_name) { }
+};
+
+class ProductWithUnits : public Product {
+	int unit_id;
+	double qty;
+
+public:
+	ProductWithUnits(std::string _name, int u_id, double _qty): Product(_name), unit_id(u_id), qty(_qty) { }
+};
