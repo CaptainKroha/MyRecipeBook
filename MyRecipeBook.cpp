ce2f51024cb74cb3d445eda9c245278efb5455c9
CaptainKroha <dzokvinton@gmail.com>
2024-06-15 21:43:52 +0300

    Add menu logic in main()

--- MyRecipeBook.cpp
+++ MyRecipeBook.cpp
@@ -1,12 +1,14 @@
 ﻿# include <iostream>
 # include "dbclass.h"
 # include <sqlite3.h>
-# include <string>
-# include "visualizer.h"
-# include <windows.h>
+#include <string>
+#include "visualizer.h"
+#include <windows.h>
+#include "manager.h"
 
 const char* path_head = "D:/my_dir_for_learning/projects/MyRecipeBook/";
 const char* main_menu[]{ "Добавить рецепт", "Найти рецепт", "Удалить рецепт", "Открыть всю книгу" };
+enum {add_recipe = 1, find_recipe = 2, pop_recipe = 3, open_book = 4, exit = 0};
 
 int main()
 {
@@ -22,8 +24,20 @@ int main()
 	catch(int) {
 		std::cerr << "Ошибка открытия базы данных" << std::endl;
 	}
+	RecipesManager* manager = new RecipesManager(connection);
 	VisualizerBase* visualizer = new ListVisualizer;
 	int res = visualizer->show_menu(main_menu, sizeof(main_menu)/sizeof(main_menu[0]));
-	std::cout << res;
+	switch (res) {
+	case add_recipe:
+	
+	case find_recipe:
+
+	case pop_recipe:
+
+	case open_book:
+
+	case exit:
+
+	}
 	return 0;
 }
