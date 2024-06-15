8bbb09f32207a16d9a683a28e9004716276c859d
CaptainKroha <dzokvinton@gmail.com>
2024-06-15 14:11:47 +0300

    Add project files.

---
+++ MyRecipeBook.cpp
@@ -0,0 +1,22 @@
+﻿# include <iostream>
+# include "dbclass.h"
+# include <sqlite3.h>
+# include <string>
+
+const char* path_head = "D:/my_dir_for_learning/projects/MyRecipeBook/";
+
+int main()
+{
+	setlocale(0, "RU_ALL");
+	std::string path{ path_head };
+	path += "dbfiles/recipe_book_db.db";
+	try {
+		
+		SQLiteDBConnection* connection = new SQLiteDBConnection(path);
+		connection->open();
+	}
+	catch(int) {
+		std::cerr << "Ошибка открытия базы данных" << std::endl;
+	}
+	return 0;
+}
---
+++ dbclass.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <string>
+#include <string.h>
+
+class DBConnectionBase {
+protected:
+	std::string path{ "" };
+	DBConnectionBase(std::string& _path) {
+		path = _path;
+	}
+public:
+	virtual void open() = 0;
+};
+
+class SQLiteDBConnection: public DBConnectionBase {
+	sqlite3* db{ nullptr };
+
+public:
+	SQLiteDBConnection(std::string& _path) : DBConnectionBase(_path)
+		{ }
+	~SQLiteDBConnection() {
+		if(db) sqlite3_close(db);
+	}
+	virtual void open() override
+	{
+		int rc = sqlite3_open(path.c_str(), &db);
+		if (rc) throw 1;
+	}
+
+	
+};
