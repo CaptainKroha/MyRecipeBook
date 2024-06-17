85d65b92460a5ed78253e99f563df49245bf1c2d
CaptainKroha <dzokvinton@gmail.com>
2024-06-17 07:28:19 +0300

    Install UTF-8

--- MyRecipeBook.cpp
+++ MyRecipeBook.cpp
@@ -5,14 +5,23 @@
 #include "visualizer.h"
 #include <windows.h>
 #include "manager.h"
+#include <locale>
+#include <vector>
 
 const char* path_head = "D:/my_dir_for_learning/projects/MyRecipeBook/";
-const char* main_menu[]{ "Добавить рецепт", "Найти рецепт", "Удалить рецепт", "Открыть всю книгу" };
+std::vector<std::wstring> main_menu { L"Добавить рецепт", L"Найти рецепт", L"Удалить рецепт", L"Открыть всю книгу"};
 enum {add_recipe = 1, find_recipe = 2, pop_recipe = 3, open_book = 4, out = 0};
 
 int main()
 {
-	system("chcp 1251>nul");
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	std::setlocale(LC_ALL, "ru_RU.UTF-8");
+	std::wcin.imbue(std::locale("ru_RU.UTF-8"));
+	std::wcout.imbue(std::locale("ru_RU.UTF-8"));
+	
+	// system("chcp 1251>nul");
 	std::string path{ path_head };
 	path += "dbfiles/recipe_book_db.db";
 	SQLiteDBConnection* connection{ nullptr };
@@ -26,7 +35,7 @@ int main()
 
 	RecipesManager* manager = new RecipesManager(connection);
 	VisualizerBase* visualizer = new ListVisualizer;
-	int res = visualizer->show_menu(main_menu, sizeof(main_menu)/sizeof(main_menu[0]));
+	int res = visualizer->show_list(main_menu);
 	switch (res) {
 	case add_recipe:
 		manager->create_recipe();
@@ -41,4 +50,4 @@ int main()
 		break;
 	}
 	return 0;
-}
+ }
