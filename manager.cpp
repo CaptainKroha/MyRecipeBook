add18e8ab60f5a0762185f804fdac21b772de8f0
CaptainKroha <dzokvinton@gmail.com>
2024-06-17 07:35:41 +0300

    Add get methods for InputManager

--- manager.cpp
+++ manager.cpp
@@ -27,51 +27,90 @@ void RecipesManager::create_recipe()
 
 }
 
+Recipe* InputManager::get_recipe(DBConnectionBase* db)
+{
+	system("cls");
+	std::wstring name, recipe_text;
+	std::list<ProductWithUnits> pwu_list;
+	/*
+	* “ут надо запомнить позицию указател€, после чего передавать ее в функцию чтени€ названи€,
+	* если название введено некорректно возращать пользовател€ на это место
+	*/
+	name = get_recipe_name(db);
+	std::wcout << L"¬ведите список продуктов: " << std::endl;
+	get_pwu_list(db, pwu_list);
+	// std::wcout << L"¬ведите текст рецепта: ";
+	return nullptr;
+}
+
 int InputManager::get_int()
 {
-	std::string tmp;
-	std::cin >> tmp;
-	if (tmp == "0") return 0;
-	return atoi(tmp.c_str());
+	std::wstring tmp;
+	std::wcin >> tmp;
+	if (tmp == L"0") return 0;
+	return atoi(wchar_to_utf8(tmp.c_str()).c_str());
 }
-std::string InputManager::get_str()
+
+int InputManager::get_int(std::wstring addition)
 {
-	std::string tmp;
-	std::cin.ignore(1024, '\n');
-	std::getline(std::cin, tmp);
+	std::wcout << addition;
+	return get_int();
+}
+
+std::wstring InputManager::get_str()
+ {
+	std::wstring tmp;
+	if(std::wcin.peek() == L'\n')
+		std::wcin.ignore(1024, L'\n');
+	std::getline(std::wcin, tmp);
 	return tmp;
 }
+
+std::wstring InputManager::get_str(std::wstring addition)
+{
+	std::wcout << addition;
+	return get_str();
+}
+
 std::string InputManager::get_recipe_text()
 {
 	return std::string("");
 }
-std::string InputManager::get_recipe_name(DBConnectionBase* db)
+
+std::wstring InputManager::get_recipe_name(DBConnectionBase* db)
 {
-	std::vector<std::string> names;
-	db->get_recipes_names(names);
-	std::string tmp;
+	std::vector<std::wstring> names;
+	db->get_names(L"recipe", names);
+	std::wstring tmp;
 	do {
-		tmp = get_str();
+		tmp = get_str(L"¬ведите название рецепта: ");
 		if (names.empty() == true) break;
 		auto it = std::find(names.begin(), names.end(), tmp);
 		if (it != names.end()) break;
 	} while (true);
 	return tmp;
 }
-Recipe* InputManager::get_recipe(DBConnectionBase* db)
+
+ProductWithUnits* InputManager::get_pwu(DBConnectionBase* db, std::vector<std::wstring>& names, std::vector<std::wstring>& units)
 {
+	VisualizerBase* visualizer = new ListVisualizer;
+	std::wstring name;
+	int unit_id;
+	double qty;
+	// ввод названи€ продукта динамически
+	name = get_str(L"¬ведите название продукта: ");
+	unit_id = visualizer->show_list(units);
+	unit_id = db->get_id_by_name(L"units", units[unit_id - 1]);
 	system("cls");
-	std::cout << "¬ведите название рецепта: ";
-	/* 
-	* “ут надо запомнить позицию указател€, после чего передавать ее в функцию чтени€ названи€,
-	* если название введено некорректно возращать пользовател€ на это место 
-	*/
-	std::string name = get_recipe_name(db);
-	std::cout << "¬ведите список продуктов: ";
-	return nullptr;
+	qty = get_int(L"¬ведите количество ингредиента: ");
+	std::wcout << name << " " << unit_id << " " << qty << std::endl;
+	return new ProductWithUnits(name, unit_id, qty);
 }
 
-ProductWithUnits* InputManager::get_pwu(DBConnectionBase* db)
+void InputManager::get_pwu_list(DBConnectionBase* db, std::list<ProductWithUnits>& pwu_list)
 {
-	return nullptr;
+	std::vector<std::wstring> names, units;
+	db->get_names(L"products", names);
+	db->get_names(L"units", units);
+	pwu_list.push_back(*get_pwu(db, names, units));
 }
--- manager.h
+++ manager.h
@@ -3,6 +3,7 @@
 #include "items.h"
 #include <string>
 #include <vector>
+#include <list>
 
 class RecipesManager {
 	DBConnectionBase* db{ nullptr };
@@ -17,9 +18,14 @@ public:
 class InputManager {
 public:
 	static int get_int();
-	static std::string get_str();
+	static int get_int(std::wstring addition);
+	static std::wstring get_str();
+	static std::wstring get_str(std::wstring);
 	static std::string get_recipe_text();
-	static std::string get_recipe_name(DBConnectionBase* db);
+	static std::wstring get_recipe_name(DBConnectionBase* db);
 	static Recipe* get_recipe(DBConnectionBase* db);
-	static ProductWithUnits* get_pwu(DBConnectionBase* db);
+	static ProductWithUnits* get_pwu(DBConnectionBase* db, std::vector<std::wstring>& names, std::vector<std::wstring>& units);
+	static void get_pwu_list(DBConnectionBase* db, std::list<ProductWithUnits>& pwu_list);
+	static std::string wchar_to_utf8(const wchar_t* wstr);
+	static std::wstring utf8_to_wchar(const char* str);
 };
