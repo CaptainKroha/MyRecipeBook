df51424eca73dfd79147127d71673700b7876bc1
CaptainKroha <dzokvinton@gmail.com>
2024-06-17 07:30:46 +0300

    Update items fields to wstring

--- items.h
+++ items.h
@@ -4,10 +4,10 @@
 
 
 class Product {
-	std::string name;
+	std::wstring name;
 
 public:
-	Product(std::string _name):name(_name) { }
+	Product(std::wstring _name):name(_name) { }
 };
 
 class ProductWithUnits : public Product {
@@ -15,16 +15,16 @@ class ProductWithUnits : public Product {
 	double qty;
 
 public:
-	ProductWithUnits(std::string _name, int u_id, double _qty): Product(_name), unit_id(u_id), qty(_qty) { }
+	ProductWithUnits(std::wstring _name, int u_id, double _qty): Product(_name), unit_id(u_id), qty(_qty) { }
 };
 
 class Recipe {
 	int id{ -1 };
-	std::string name{""};
+	std::wstring name{L""};
 	std::list<ProductWithUnits> products_list;
-	std::string recipe{""};
+	std::wstring recipe{L""};
 public:
 	Recipe() { }
-	Recipe(std::string _name, std::string _recipe) : name(_name), recipe(_recipe) { }
-	Recipe(int _id, std::string _name, std::string _recipe):id(_id), name(_name), recipe(_recipe) { }
+	Recipe(std::wstring _name, std::wstring _recipe) : name(_name), recipe(_recipe) { }
+	Recipe(int _id, std::wstring _name, std::wstring _recipe):id(_id), name(_name), recipe(_recipe) { }
 };
