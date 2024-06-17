ac2035e8b2b67d4f56501f305d51cc343fa226bd
CaptainKroha <dzokvinton@gmail.com>
2024-06-17 07:29:59 +0300

    Add show_list() for string classes

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -15,6 +15,26 @@ int ListVisualizer::show_list(const char* list[], int size)
 
 }
 
+int ListVisualizer::show_list(std::vector<std::string>& list) {
+	system("cls");
+	for (int i = 0; i < list.size(); i++) {
+		std::cout << i + 1 << ". " << list[i] << std::endl;
+	}
+	std::cout << "-> ";
+	int res = InputManager::get_int();
+	return res;
+}
+
+int ListVisualizer::show_list(std::vector<std::wstring>& list) {
+	system("cls");
+	for (int i = 0; i < list.size(); i++) {
+		std::wcout << i + 1 << L". " << list[i] << std::endl;
+	}
+	std::wcout << L"-> ";
+	int res = InputManager::get_int();
+	return res;
+}
+
 int ListVisualizer::show_menu(const char* list[], int size)
 {
 	system("cls");
--- visualizer.h
+++ visualizer.h
@@ -1,12 +1,18 @@
 #pragma once
+#include <vector>
+#include <string>
 
 class VisualizerBase {
 public:
 	virtual int show_list(const char* [], int size) = 0;
+	virtual int show_list(std::vector<std::string>& list) = 0;
+	virtual int show_list(std::vector<std::wstring>& list) = 0;
 	virtual int show_menu(const char* [], int size) = 0;
 };
 
 class ListVisualizer :public VisualizerBase {
 	virtual int show_list(const char* list[], int size) override;
+	virtual int show_list(std::vector<std::string>& list) override;
+	virtual int show_list(std::vector<std::wstring>& list) override;
 	virtual int show_menu(const char* list[], int size) override;
 };
